739676d9f467b59e0bc1f4bab2dd66d98e7a9232
myantyuWorld <nepinepi984@gmail.com>
2024-08-14 07:53:55 +0900

    カテゴリ一覧ページ

---
+++ front/vue_app/src/features/categories/components/CategoryItem.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import BatsuButton from "@/components/elements/button/BatsuButton.vue";
+
+defineProps<{
+  label: string;
+}>();
+</script>
+
+<template>
+  <li>
+    {{ label }}
+    <BatsuButton
+      @click="
+        () => {
+          console.log('hello category item click!');
+        }
+      "
+    ></BatsuButton>
+  </li>
+</template>
