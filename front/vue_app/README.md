4b7f79c97de6c1e8fe9e190a257aacb594ba4dee
myantyuWorld <nepinepi984@gmail.com>
2024-08-15 15:51:43 +0900

    modify frontend directory

--- front/vue_app/README.md
+++
@@ -1,6 +0,0 @@
-# 使用ライブラリ
-- Vite
-- vue.js
-- vue-router
-- axios
-- tailwindcss
--- front/vue_app/__tests__/setupFile.ts
+++
@@ -1,6 +0,0 @@
-import { server } from "./node.js";
-import { afterAll, afterEach, beforeAll } from "vitest";
-
-beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
-afterEach(() => server.resetHandlers())
-afterAll(() => server.close())
